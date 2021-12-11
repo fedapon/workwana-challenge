b7db9e56f021420192b5ddf0b0300d736448dd0a
fedapon <fedapon@gmail.com>
2021-12-11 00:51:19 -0300

    redis connection and joining and status

--- backend_node/controllers/issue.controllers.js
+++ backend_node/controllers/issue.controllers.js
@@ -1,13 +1,31 @@
-function join (req, res) {
-    res.json({messaje: 'join'})
+import issueRepositori from '../../backend_node/repositories/issue.repositories.js'
+import issueRepositories from '../../backend_node/repositories/issue.repositories.js'
+
+async function join (req, res) {
+    let data = await issueRepositories.getIssue(req.params.issue)
+    if (data == null) {
+        data = {
+            status : 'joining',
+            members: []
+        }
+    }
+
+    let newMember = {
+        name: req.body.name,
+        status: 'waiting'
+    }
+    data.members.push(newMember)
+    await issueRepositories.setIssue(req.params.issue, data)
+    
+    res.json(data)
 }
 
 function vote (req, res) {
     res.json({messaje: 'vote'})
 }
 
-function status (req, res) {
-    res.json({messaje: 'status'})
+async function status (req, res) {
+    res.json(await issueRepositori.getIssue(req.params.issue))
 }
 
 export default {join, vote, status}
---
+++ backend_node/services/redis.services.js
@@ -0,0 +1,4 @@
+import ioredis from "ioredis"
+const redisConnection = new ioredis(6379, "localhost")
+
+export default redisConnection
