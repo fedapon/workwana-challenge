3c972297ad5a72b0545a3ff27cf95ed83856536b
fedapon <fedapon@gmail.com>
2021-12-11 18:13:59 -0300

    join controller returns a json token with the issue and user id

--- backend_node/controllers/issue.controllers.js
+++ backend_node/controllers/issue.controllers.js
@@ -1,32 +1,53 @@
-import issueRepository from "../../backend_node/repositories/issue.repository.js"
+import issueRepository from '../../backend_node/repositories/issue.repository.js'
+import jwt from 'jsonwebtoken'
 
 async function join(req, res) {
-    let data = await issueRepository.getIssue(req.params.issue)
+    let issue = req.params.issue
+    let data = await issueRepository.getIssue(issue)
     if (data == null) {
         data = {
-            status: "joining",
+            status: 'joining',
             members: [],
         }
     }
 
     let newMember = {
+        id: data.members.length + 1,
         name: req.body.name,
-        status: "waiting",
+        status: 'waiting',
     }
     data.members.push(newMember)
-    await issueRepository.setIssue(req.params.issue, data)
+    await issueRepository.setIssue(issue, data)
 
-    return res.json(data)
+    let token = await generateToken(issue, newMember.id)
+
+    return res.json({ ...newMember, token })
+}
+
+async function generateToken(issue, userId) {
+    return new Promise(function (resolve, reject) {
+        jwt.sign(
+            { issue, userId },
+            process.env.SECRET,
+            { expiresIn: '1d' },
+            (error, token) => {
+                if (error) {
+                    reject(error)
+                }
+                resolve(token)
+            }
+        )
+    })
 }
 
 function vote(req, res) {
-    return res.json({ messaje: "vote" })
+    return res.json({ messaje: 'vote' })
 }
 
 async function status(req, res) {
     let data = await issueRepository.getIssue(req.params.issue)
     if (data == null) {
-        return res.json({ messaje: "issue does not exist" })
+        return res.json({ messaje: 'issue does not exist' })
     }
     return res.json(await issueRepository.getIssue(req.params.issue))
 }
