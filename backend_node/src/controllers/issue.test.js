b6748c8f6776bc380698924a72ace07d58ce2fbe
fedapon <fedapon@gmail.com>
2021-12-29 19:50:47 -0300

    finished unit test

--- backend_node/src/controllers/issue.test.js
+++ backend_node/src/controllers/issue.test.js
@@ -1,12 +1,9 @@
-import {jest, expect, test} from '@jest/globals';
+import { jest, expect, test } from '@jest/globals'
 import {
-    generateToken,
     voteIsInvalid,
     getVoteAverage,
-    hideNotAllowedInformationToUsers,
-    emitUpdates
+    hideNotAllowedInformationToUsers
 } from './issue.helpers.controller.js'
-
 
 test('vote is valid', () => {
     let isInvalid = voteIsInvalid(1)
@@ -22,4 +19,98 @@ test('vote is invalid', () => {
     expect(isInvalid).toBe(true)
     isInvalid = voteIsInvalid(100)
     expect(isInvalid).toBe(true)
+})
+
+test('get vote average', () => {
+    const mockIssue = {
+        status: 'reveal',
+        members: [
+            {
+                id: 1,
+                name: 'Moderator',
+                role: 'moderator',
+                status: 'joined'
+            },
+            {
+                id: 2,
+                name: 'User1',
+                status: 'joined',
+                value: 20
+            },
+            {
+                id: 3,
+                name: 'User2',
+                status: 'joined',
+                value: 5
+            },
+            {
+                id: 4,
+                name: 'User3',
+                status: 'joined',
+                value: 8
+            }
+        ]
+    }
+    expect(getVoteAverage(mockIssue)).toBe(11)
+})
+
+test('hide sensitive information', () => {
+    const mockIssue = {
+        status: 'voting',
+        members: [
+            {
+                id: 1,
+                name: 'Moderator',
+                role: 'moderator',
+                status: 'joined'
+            },
+            {
+                id: 2,
+                name: 'User1',
+                status: 'joined',
+                value: 20
+            },
+            {
+                id: 3,
+                name: 'User2',
+                status: 'joined',
+                value: 5
+            },
+            {
+                id: 4,
+                name: 'User3',
+                status: 'joined',
+                value: 8
+            }
+        ]
+    }
+    const expectedResult = {
+        status: 'voting',
+        members: [
+            {
+                id: 1,
+                name: 'Moderator',
+                role: 'moderator',
+                status: 'joined'
+            },
+            {
+                id: 2,
+                name: 'User1',
+                status: 'joined'
+            },
+            {
+                id: 3,
+                name: 'User2',
+                status: 'joined'
+            },
+            {
+                id: 4,
+                name: 'User3',
+                status: 'joined'
+            }
+        ]
+    }
+    expect(hideNotAllowedInformationToUsers(mockIssue)).toMatchObject(
+        expectedResult
+    )
 })
